a88f3b4b3a54f865e4bfa735c85a16df6805fc74
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-09-25 01:40:02 -0400

    Ai can now attack

--- Assets/Scripts/Player Movement/MouseLook.cs
+++ Assets/Scripts/Player Movement/MouseLook.cs
@@ -1,22 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
-    
-    public Transform playerBody;
-    float xRotation = 0f;
+    private float mouseSensitivity = 500f;
+    [SerializeField] private Transform playerBody;
+    private float xRotation = 0f;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
