62f12ef1326862a7b39e8f4947640fe41fa1c2cc
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-09-30 13:08:57 -0400

    Added Water Shader

--- Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -4,7 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
   public CharacterController controller;
    
-  [SerializeField] private float moveSpeed, groundedSpeed, airSpeed, floatSpeed, groundDistance, gravity;
+  [SerializeField] private float moveSpeed, groundedSpeed, airSpeed, floatSpeed, outOfWaterSpeed, 
+  groundDistance, gravityInWater, gravityOutOfWater;
   [SerializeField] private Transform groundCheck;
   [SerializeField] private LayerMask groundMask;
   private Vector3 velocity;
@@ -13,59 +14,88 @@ public class PlayerMovement : MonoBehaviour
     
   enum State
   {
-    move
+    inWater, outOfWater
   }
 
   private void Awake()
   {
-    state = State.move;
+    state = State.inWater;
   }
   private void Update()
   {
     switch (state)
     {
       default:
-        case State.move:
-              Move();
+        case State.inWater:
+              MoveInWater();
       break;
+
+        case State.outOfWater:
+              MoveOutOfWater();
+        break;
     }
+    Debug.Log(state);
   }
 
-    private void Move()
+    private void MoveInWater()
     {
-      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-    if(isGrounded)
-    {
-      moveSpeed = groundedSpeed;
-    }
-    else
-    {
-      moveSpeed = airSpeed;
-    }
-    Debug.Log(moveSpeed);
+        if(isGrounded)
+        {
+          moveSpeed = groundedSpeed;
+        }
+        else
+        {
+          moveSpeed = airSpeed;
+        }
       
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Float Up
+        if (Input.GetKey(KeyCode.Space))
+        {
+          velocity.y = floatSpeed;
+        }
+
+        //Float Down
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+          velocity.y = -floatSpeed * 2;
+        }
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+        velocity.y += gravityInWater * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void MoveOutOfWater()
+    {
+      moveSpeed = outOfWaterSpeed;
       float x = Input.GetAxis("Horizontal");
       float z = Input.GetAxis("Vertical");
-
       Vector3 move = transform.right * x + transform.forward * z;
 
-      // Float Up
-      if (Input.GetKey(KeyCode.Space))
-      {
-        velocity.y = floatSpeed;
-      }
-
-      //Float Down
-      if (Input.GetKey(KeyCode.LeftShift))
-      {
-        velocity.y = -floatSpeed * 2;
-      }
-
       controller.Move(move * moveSpeed * Time.deltaTime);
-
-      velocity.y += gravity * Time.deltaTime;
-
+      velocity.y += gravityOutOfWater * Time.deltaTime;
       controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if (other.gameObject.tag == "Water")
+      {
+        state = State.inWater;
+      }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+      if (other.gameObject.tag == "Water")
+      {
+        state = State.outOfWater;
+      }
     }
 }
