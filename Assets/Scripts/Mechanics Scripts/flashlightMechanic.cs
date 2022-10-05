d6dd11c5dfced92e7919bdaf2ad6eb7ff0994c81
F-Alex-projects <alex@franquez.com>
2022-10-04 23:51:02 -0400

    Flashlight and Blacklight Done

---
+++ Assets/Scripts/Mechanics Scripts/flashlightMechanic.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlightMechanic : MonoBehaviour
+{
+    [SerializeField] GameObject FlashlightLight;
+    [SerializeField] GameObject BlacklightLight;
+    private bool flashlightActive = false;
+    private bool blacklightActive = false;
+    public float range = 10f;
+    public Camera mainCam;
+    public LayerMask layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlashlightLight.gameObject.SetActive(false);
+        BlacklightLight.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!flashlightActive)
+            {
+                FlashlightLight.gameObject.SetActive(true);
+                flashlightActive = true;
+            }
+            else
+            {
+                FlashlightLight.gameObject.SetActive(false);
+                flashlightActive = false;
+            }
+        }
+        if (flashlightActive)
+        {
+            if (Input.GetKey(KeyCode.B))
+            {
+                BlacklightReveal();
+            }
+            else
+            {
+                Debug.Log("Blacklight off");
+                BlacklightLight.gameObject.SetActive(false);
+                FlashlightLight.gameObject.SetActive(true);
+            }
+        }
+
+    }
+
+    void BlacklightReveal()
+    {
+        BlacklightLight.gameObject.SetActive(true);
+        FlashlightLight.gameObject.SetActive(false);
+        RaycastHit hit;
+        if(Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit, range, layer))
+        {
+            Debug.Log("HiddenObjectRevealed");
+        }
+        else
+        {
+            Debug.Log("No object to reveal");
+        }
+    }
+}
