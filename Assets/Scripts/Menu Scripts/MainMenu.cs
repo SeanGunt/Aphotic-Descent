1afcf8af150b78379fc26a9c8025e22a648bbfb8
BuriedDonut <Jared.toavs@knights.ucf.edu>
2022-10-07 16:12:30 -0400

    MainMenuFunctionality

--- Assets/Scripts/Menu Scripts/MainMenu.cs
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayCodeScene ()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayArtScene ()
     {
-        
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
     }
 }
