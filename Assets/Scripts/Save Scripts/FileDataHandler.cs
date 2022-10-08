9371ca17adef58c6eda1c9a181653fd08eddcfbd
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-10-08 15:57:59 -0400

    Saving done

---
+++ Assets/Scripts/Save Scripts/FileDataHandler.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System;
+using System.IO;
+
+public class FileDataHandler
+{
+    private string dataDirPath = "";
+    private string dataFileName = "";
+
+    public FileDataHandler(string dataDirPath, string dataFileName)
+    {
+        this.dataDirPath = dataDirPath;
+        this.dataFileName = dataFileName;
+    }
+
+    public GameData Load()
+    {
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        GameData loadedData = null;
+        if (File.Exists(fullPath))
+        {
+                try
+                {
+                    string dataToLoad = "";
+                    using (FileStream stream = new FileStream(fullPath, FileMode.Open))
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            dataToLoad = reader.ReadToEnd();
+                        }
+                    }
+                    loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+                }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occured when trying to load data from the file " + fullPath + "\n" + e);
+            }
+
+        }
+        return loadedData;
+    }
+
+    public void Save(GameData data)
+    {
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+            string dataToStore = JsonUtility.ToJson(data, true);
+            
+            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error occured when trying to save data to the file" + fullPath + "\n" + e);
+        }
+    }
+}
---
+++ Assets/Scripts/Save Scripts/GameData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public Vector3 playerPosition;
+    public Quaternion playerRotation;
+    public GameData()
+    {
+        playerPosition = new Vector3(0,1,0);
+        playerRotation = new Quaternion(0,0,0,0);
+    }
+}
