0a24fe7b9f7943817b2cd86b141930022aa0b593
jval1014 <77355756+jval1014@users.noreply.github.com>
2022-10-21 15:35:22 -0400

    Door/key wokring fr

---
+++ Assets/Scripts/Interactor Script/DoorKey.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class DoorKey : MonoBehaviour {
+ 
+    public bool inTrigger;
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+ 
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+ 
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+ 
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "Press Q to take key");
+        }
+    }
+}
