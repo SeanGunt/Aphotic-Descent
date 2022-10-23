d74b04d2bf96bc30d713786da59bf3d42285c9e7
F-Alex-projects <alex@franquez.com>
2022-10-22 21:32:51 -0400

    NewInputs(OldInputSystem)

--- Assets/Scripts/Player Movement/MouseLook.cs
+++ Assets/Scripts/Player Movement/MouseLook.cs
@@ -28,7 +28,11 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX); 
         
-    
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Quit");
+        }
        
     }
 }
