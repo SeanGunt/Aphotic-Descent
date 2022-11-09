2dee08ede98852d7dd6acaba52913df71e5d1c7b
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-11-09 18:34:05 -0500

    Menu 4:3 and a bunch of fixes

--- Assets/Scripts/Menu Scripts/Menus.cs
+++ Assets/Scripts/Menu Scripts/Menus.cs
@@ -21,36 +21,12 @@ public class Menus : MonoBehaviour
         PauseMenu.SetActive(true);
         LevelSelect.SetActive(false);
     }
-
-    public void PlayCodeScene ()
-    {
-        Time.timeScale = 1.0f;
-        SceneManager.LoadScene(1);
-        DataPersistenceManager.instance.LoadGame();
-    }
-
-    public void PlayArtScene ()
-    {
-        Time.timeScale = 1.0f;
-        SceneManager.LoadScene(2);
-    }
-
+    
     public void PlayWhiteboxMaze ()
     {
         Time.timeScale = 1.0f;
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(1);
     }
-
-    public void PlaySubmarine ()
-    {
-        Time.timeScale = 1.0f;
-        SceneManager.LoadScene(4);
-    }
-
-    //public void PlayReef ()
-    //{
-        //SceneManager.LoadScene(5);
-    //}
     public void ResumeGame()
     {
         PauseMenu.SetActive(false);
