c411179e95a6fa0b83cea3ddfffec2ea87aef18e
F-Alex-projects <alex@franquez.com>
2022-11-13 18:48:09 -0500

    MenuFixesPart2

--- Assets/Scripts/Menu Scripts/Menus.cs
+++ Assets/Scripts/Menu Scripts/Menus.cs
@@ -2,24 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Menus : MonoBehaviour
 {
     public GameObject Player;
     public GameObject PauseMenu;
     public GameObject LevelSelect;
-    [SerializeField] private GameObject gameUI;
+    public GameObject SettingsMenu;
+    [SerializeField] private GameObject gameUI, levelSelectFirstButton, levelSelectClosedButton, settingsFirstButton, settingsClosedButton;
+    
 
     public void OpenLevelSelect()
     {
         PauseMenu.SetActive(false);
         LevelSelect.SetActive(true);
+        Player.GetComponent<PauseControls>().otherMenuActive = true;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(levelSelectFirstButton);
     }
 
-    public void BackButton()
+    public void CloseLevelSelect()
     {
         PauseMenu.SetActive(true);
         LevelSelect.SetActive(false);
+        Player.GetComponent<PauseControls>().otherMenuActive = false;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(levelSelectClosedButton);
+    }
+
+    public void OpenSettings()
+    {
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+        Player.GetComponent<PauseControls>().otherMenuActive = true;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(settingsFirstButton);
+    }
+
+    public void CloseSettings()
+    {
+        PauseMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+        Player.GetComponent<PauseControls>().otherMenuActive = false;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(settingsClosedButton);
     }
     
     public void PlayWhiteboxMaze ()
