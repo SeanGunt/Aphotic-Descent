6546b0158796345c818fe8547e8b4c911e8d3e5b
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-11-17 22:31:08 -0500

    Shaders and Particles

--- Assets/Scripts/Mechanics Scripts/OpenSecondDoor.cs
+++ Assets/Scripts/Mechanics Scripts/OpenSecondDoor.cs
@@ -6,10 +6,20 @@ public class OpenSecondDoor : MonoBehaviour
 {
     [SerializeField] GameObject door;
     [SerializeField] flashlightMechanic fmech;
+
+    private void Start()
+    {
+        if (GameDataHolder.secondDoorOpened)
+        {
+            door.transform.eulerAngles = new Vector3(0,-90,0);
+        }
+    }
     public void Open()
     {
         door.transform.eulerAngles = new Vector3(0,-90,0);
         fmech.flashlightText.text = "Door Opened";
         fmech.Invoke("ClearUI", 3);
+        GameDataHolder.secondDoorOpened = true;
+        DataPersistenceManager.instance.SaveGame();
     }
 }
