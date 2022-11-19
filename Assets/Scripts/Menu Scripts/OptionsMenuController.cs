c37f98181562a11ed364b10e02fd570685ad2c1c
F-Alex-projects <alex@franquez.com>
2022-11-19 02:30:42 -0500

    Save to playerPrefs (no full functionality yet)

--- Assets/Scripts/Menu Scripts/OptionsMenuController.cs
+++ Assets/Scripts/Menu Scripts/OptionsMenuController.cs
@@ -5,15 +5,23 @@ using UnityEngine.UI;
 
 public class OptionsMenuController : MonoBehaviour
 {
+    public bool initialized = false;
+    public Slider mouseSensitivitySlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("Sensitivity"))
+        {
+            mouseSensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity");
+            Debug.Log("Loaded a sensitivity of " + mouseSensitivitySlider.value);
+        }
     }
 
     //In Progress, do not delete or mess with this.
     public void SetMouseSensitivity(float val)
     {
+        if (!initialized) return;
         if (!Application.isPlaying) return;
 
         PlayerPrefs.SetFloat("Sensitivity", val);
