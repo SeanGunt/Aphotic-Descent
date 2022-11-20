4600ae5573f9c290c7c6edcea83dfc9ce4448292
F-Alex-projects <alex@franquez.com>
2022-11-20 17:09:13 -0500

    UnfinishedFlashlightStuff

--- Assets/Scripts/Mechanics Scripts/RevealHiddenObjects.cs
+++ Assets/Scripts/Mechanics Scripts/RevealHiddenObjects.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RevealHiddenObjects : MonoBehaviour
 {
     public GameObject[] objectsToBeRevealed;
-    public bool objRevealed;
+    public bool objRevealed, objSpawned, canSpawnObjects;
+    private SpawnHiddenObject sHO;
     [SerializeField] private float revealedTime, maxRevealed;
 
     void Awake()
@@ -13,9 +14,47 @@ public class RevealHiddenObjects : MonoBehaviour
         objRevealed = false;
     }
 
+    void Start()
+    {
+        if (this.GetComponent<SpawnHiddenObject>() != false)
+        {
+            sHO = this.GetComponent<SpawnHiddenObject>();
+            canSpawnObjects = true;
+        }
+        else canSpawnObjects = false;
+    }
+
     void Update()
     {
         revealedTime = Mathf.Clamp(revealedTime, 0f, maxRevealed);
+        if (objSpawned == false)
+        {
+            if (canSpawnObjects)
+            {
+                RevealObject();
+                objSpawned = sHO.objSpawned;
+            }
+            else
+            {
+                RevealObject();
+            }
+        }
+        
+        
+    }
+
+    void RevelationTimer()
+    {
+        revealedTime -= Time.deltaTime;
+        if (revealedTime <= 0)
+        {
+            objRevealed = false;
+            revealedTime = maxRevealed;
+        }
+    }
+
+    void RevealObject()
+    {
         if (objRevealed)
         {
             foreach (GameObject objectToBeRevealed in objectsToBeRevealed)
@@ -26,16 +65,6 @@ public class RevealHiddenObjects : MonoBehaviour
         {
             foreach (GameObject objectToBeRevealed in objectsToBeRevealed)
             objectToBeRevealed.gameObject.SetActive(false);
-        }
-    }
-
-    void RevelationTimer()
-    {
-        revealedTime -= Time.deltaTime;
-        if (revealedTime <= 0)
-        {
-            objRevealed = false;
-            revealedTime = maxRevealed;
         }
     }
 }
