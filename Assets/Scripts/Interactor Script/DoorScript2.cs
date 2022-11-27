df6313724e7f03bb8f64ada1ac2e9f30d478c737
F-Alex-projects <alex@franquez.com>
2022-11-27 03:05:22 -0500

    Beginnings of door

---
+++ Assets/Scripts/Interactor Script/DoorScript2.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript2 : MonoBehaviour
+{
+    public bool open;
+    public bool close;
+    public bool inTrigger;
+    private BoxCollider bCollider;
+    private UItext uItext;
+
+    private void Awake()
+    {
+        inTrigger = false;
+        bCollider = GetComponent<BoxCollider>();
+        uItext = GetComponent<UItext>();
+    }
+ 
+    void OnTriggerEnter(Collider other)
+    {
+         if (other.gameObject.tag == "Player") 
+         {
+             inTrigger = true;
+         }
+    }
+ 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+         {
+            inTrigger = false;
+         }
+    }
+ 
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (close)
+            {
+                if (GameDataHolder.doorKey)
+                {
+                    if (Input.GetButtonDown("Interact"))
+                    {
+                        bCollider.enabled = false;
+                        uItext.GuiOn = false;
+                        open = true;
+                        close = false;
+                    }
+                }
+            }
+        }
+ 
+        if (open)
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -180.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+    }
+}
