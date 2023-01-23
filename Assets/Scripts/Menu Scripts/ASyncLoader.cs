2e27bbf2c0691351c6c34d88f51eccfd1fff7377
jval1014 <77355756+jval1014@users.noreply.github.com>
2023-01-22 21:53:59 -0500

    Loading UI done

---
+++ Assets/Scripts/Menu Scripts/ASyncLoader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class ASyncLoader : MonoBehaviour
+{
+   
+}
