bfd8e648092e8a578c87ca7de9e84a5bb00bd59e
flowering-knight01 <ajrose428@knights.ucf.edu>
2023-02-04 19:01:22 -0500

    pistolShrimpShootingDone

--- Assets/Scripts/Ai Scripts/psEnemyAI.cs
+++ Assets/Scripts/Ai Scripts/psEnemyAI.cs
@@ -40,8 +40,6 @@ public class psEnemyAI : MonoBehaviour
         shootTimerReset = shootTimer;
 
         resetSpeed = psAgent.speed;
-
-        
     }
 
     // Update is called once per frame
@@ -50,14 +48,11 @@ public class psEnemyAI : MonoBehaviour
         playerLocation = thePlayer.transform.position;
 
         distBtwn = Vector3.Distance(playerLocation, this.transform.position);
-        //Debug.Log(distBtwn + " is the dist btwn");
-        //Debug.Log("remaining dist is: " + psAgent.remainingDistance);
-        //Debug.Log("pathpending is: " + psAgent.pathPending);
 
         if(unchosen)
         {
             //currently the same as the freakfish
-            //if you see this I've forgotten to remove the comments
+            //if you see this I've forgotten to remove this comment
 
             moveToPoint();
         }
@@ -89,8 +84,7 @@ public class psEnemyAI : MonoBehaviour
             shootTimer -= Time.deltaTime;
             if(shootTimer <= 0)
             {
-                Debug.Log("this is the part where it kills you");
-                shoot();
+                gunRotating.shoot();
                 shootTimer = shootTimerReset;    
             }
         }
@@ -98,14 +92,10 @@ public class psEnemyAI : MonoBehaviour
         {
             shootTimer = shootTimerReset;
         }
-
-        shoot();
     }
 
     void moveToPoint()
     {
-        Debug.Log("ps moving to new point");
-
         unchosen = false;
         psAgent.destination = travelPoints[Random.Range(0, travelPoints.Length)].position;
     }
@@ -115,34 +105,25 @@ public class psEnemyAI : MonoBehaviour
         //the plan is to rotate the gunAimer at the player when this is active
         //then check if gunRayCast is detecting the player, and if so, shoot
         //else stand and wait?
-        Debug.Log("hey in range");
+        
         inShootRange = true;
 
         gunRotating.rotateToPlayer = true;
-
-
     }
 
-    void shoot()
-    {
-        //uses raycast
-        Vector3 centerRay = transform.TransformDirection(Vector3.forward) * gunRange;
-        Debug.DrawRay(psGunHead.transform.position, centerRay, Color.red);
-
-        if(Physics.Raycast(psGunHead.transform.position, centerRay, out hit, gunRange))
-        {
-            Debug.Log(hit.collider.gameObject.name + " was hit");
-        }
-    }
-
-    /*
+    /*  
     void findClosestPoint()
     {
-        closestPoint = Mathf.Infinity;
+        //closestPoint = Mathf.Infinity;
+        Transform closestPoint;
 
         foreach(Transform currentPoint in travelPoints)
         {
             closestPoint = (currentPoint.position - thePlayer.transform.position).sqrMagnitude;
+            if(closestPoint > currentPoint)
+            {
+                currentPoint = closestPoint;
+            }
         }
     }
     */
