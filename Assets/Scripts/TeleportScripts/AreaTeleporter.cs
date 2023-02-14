57f6ec7c3755b4a4de45eb6678fcb00df1c990bc
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-02-13 22:41:11 -0500

    Buyild sone

---
+++ Assets/Scripts/TeleportScripts/AreaTeleporter.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AreaTeleporter : MonoBehaviour
+{
+    private GameObject player;
+    [SerializeField] private Vector3[] positions;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        if(Keyboard.current.digit1Key.isPressed)
+        {
+            Teleport(0);
+        }
+
+        if(Keyboard.current.digit2Key.isPressed)
+        {
+            Teleport(1);
+        }
+
+        if(Keyboard.current.digit3Key.isPressed)
+        {
+            Teleport(2);
+        }
+
+        if(Keyboard.current.digit4Key.isPressed)
+        {
+            Teleport(3);
+        }
+
+        if(Keyboard.current.digit5Key.isPressed)
+        {
+            Teleport(4);
+        }
+
+        if(Keyboard.current.digit6Key.isPressed)
+        {
+            Teleport(5);
+        }
+    }
+
+    private void Teleport(int arrayPosition)
+    {
+        player.transform.localPosition = positions[arrayPosition];
+    }
+}
