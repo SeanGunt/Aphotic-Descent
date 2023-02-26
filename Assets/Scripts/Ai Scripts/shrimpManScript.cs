ea07f03d43b61c75b7e68f1adbef40a71fcc9f44
F-Alex-projects <alex@franquez.com>
2023-02-26 18:57:43 -0500

    A shrimp fried this rice

--- Assets/Scripts/Ai Scripts/shrimpManScript.cs
+++ Assets/Scripts/Ai Scripts/shrimpManScript.cs
@@ -1,18 +1,156 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class shrimpManScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent shrimpAgent;
+    [SerializeField] public float agentSpeed;
+    [SerializeField] private float detectionRange;
+    [SerializeField] private float rangeForBleedMultiplier;
+    [SerializeField] private GameObject player, shrimpMesh;
+    [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] public float stunTime;
+
+    private float bleedRange;
+    private float rangeUsed;
+    private float baseAttackTime = 2.0f;
+    Vector3 destination;
+    private float playerDistance;
+    private bool unchosen = true;
+    PlayerHealthController pHC;
+    [HideInInspector] public bool currentlyAttacking = false;
+    
+    private State state;
+
+    public enum State
     {
-        
+        attacking, patrolling, wasAttacking
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        shrimpAgent = GetComponent<NavMeshAgent>();
+        shrimpAgent.speed = agentSpeed;
+        shrimpAgent.updateRotation = true;
+        shrimpAgent.autoBraking = false;
+        shrimpAgent.acceleration = 250;
+        shrimpAgent.angularSpeed = 250;
+
+        state = State.patrolling;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(player != null)
+        {
+            //pHC = player.GetComponent<PlayerHealthController>();
+        }
+
+        bleedRange = detectionRange * rangeForBleedMultiplier;
+    }
+
     void Update()
     {
-        
+        playerDistance = (player.transform.position - this.transform.position).sqrMagnitude;
+        switch (state)
+        {
+            default:
+            case State.patrolling:
+                Patrolling();
+                break;
+            case State.attacking:
+                Attacking();
+                break;
+            case State.wasAttacking:
+                WasAttacking();
+                break;
+        }
+        // if(pHC.isBleeding)
+        // {
+        //     rangeUsed = bleedRange;
+        // }
+        // else
+        // {
+        //     rangeUsed = detectionRange;
+        // }
+
+        if((!shrimpAgent.pathPending && shrimpAgent.remainingDistance < 0.5f) && !currentlyAttacking)
+        {
+            unchosen = true;
+            state = State.patrolling;
+        }
+    }
+
+    void Patrolling()
+    {
+        if(patrolPoints.Length == 0)
+        {
+            return;
+        }
+
+        if(unchosen == true)
+        {
+            destination = patrolPoints[Random.Range(0, patrolPoints.Length)].position;
+            unchosen = false;
+        }
+
+        shrimpAgent.destination = destination;
+
+        if(playerDistance < rangeUsed*rangeUsed)
+        {
+            state = State.attacking;
+        }
+    }
+
+    void Attacking()
+    {
+        shrimpAgent.destination = player.transform.position;
+        currentlyAttacking = true;
+        baseAttackTime -= Time.deltaTime;
+
+        if(playerDistance > rangeUsed*rangeUsed && baseAttackTime <= 0f)
+        {
+            baseAttackTime = 2.0f;
+            state = State.wasAttacking;
+        }
+    }
+
+    void WasAttacking()
+    {
+        currentlyAttacking = false;
+        state = State.patrolling;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            shrimpAgent.speed = 0;
+            BreathingManager.instance.StopBreathe();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Mud")
+        {
+            shrimpAgent.speed = (agentSpeed*2.0f);
+            shrimpMesh.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 10, this.transform.position.z);
+            Debug.Log("Entered Mud");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Mud")
+        {
+            shrimpAgent.speed = agentSpeed;
+            shrimpMesh.transform.position = this.transform.position;
+            Debug.Log("Exited Mud");
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangeUsed);
     }
 }
