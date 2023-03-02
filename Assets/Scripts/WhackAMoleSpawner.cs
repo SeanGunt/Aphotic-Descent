132af4dd4a6b0f86adc5879a6cba4624367827b0
jval1014 <77355756+jval1014@users.noreply.github.com>
2023-03-02 12:36:02 -0500

    Added Whack a crab a spawner

---
+++ Assets/Scripts/WhackAMoleSpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhackAMoleSpawner : MonoBehaviour
+{
+   public Transform[] spawnLocations;
+   
+   public GameObject[] Crab;
+
+
+   
+}
