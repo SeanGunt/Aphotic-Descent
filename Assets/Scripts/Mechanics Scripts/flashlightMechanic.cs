e19ad662b65c6c239025efdb24b7676dc1876963
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-03-03 18:11:50 -0500

    Bing Bong Blacklight is new (ahahahahh) also weird lights?????

--- Assets/Scripts/Mechanics Scripts/flashlightMechanic.cs
+++ Assets/Scripts/Mechanics Scripts/flashlightMechanic.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
@@ -9,22 +7,14 @@ public class flashlightMechanic : MonoBehaviour
     [SerializeField] GameObject FlashlightLight;
     [SerializeField] GameObject BlacklightLight;
     [HideInInspector] public static RaycastHit hit;
-    private bool flashlightOn = false;
-    private bool blacklightIsOn = false;
     private PlayerInput playerInput;
-    [SerializeField]public float flashlightBattery, maxBattery;
-    [SerializeField]private Image batteryBar, fullBatteryBar, emptyBatteryBar, flashlightUI, emptyLight, onLight, blLight;
+    [SerializeField]private Image blacklightBar, flashlightUI, emptyLight, onLight, blLight;
     [SerializeField]public Text flashlightText;
-    [SerializeField]private HiddenObjectsInteraction hI;
-    [SerializeField]private RevealHiddenObjects rHO;
-    [SerializeField]private SpawnHiddenObject sHO;
-    [SerializeField]private float flashlightCooldown = 15.0f;
-    [SerializeField]private GameObject flashlightContainers;
-    private float flCdResetTime;
+    private HiddenObjectsInteraction hI;
+    private RevealHiddenObjects rHO;
+    private SpawnHiddenObject sHO;
     private BlacklightEvent blEvent;
-    private bool flashlightDisable;
-    public bool flashlightEmpty;
-    public float range = 10f;
+    public float range = 10f, blacklightChargeTime, blacklightDischargeTime;
     public Camera mainCam;
     public LayerMask layer;
     public AudioSource audioSource;
@@ -33,120 +23,120 @@ public class flashlightMechanic : MonoBehaviour
     public AudioClip blacklightOnSound;
     private GameObject player;
     private PlayerMovement PMS;
+    private State state;
+
+    private enum State
+    {
+        blacklightOn, flashlightOn, flashlightOff
+    }
 
     void Awake()
     {
         player = GameObject.FindWithTag("Player");
         playerInput = player.GetComponent<PlayerInput>();
+        state = State.flashlightOff;
     }
     
-    // Start is called before the first frame update
     void Start()
     {
         PMS = player.GetComponent<PlayerMovement>();
         FlashlightLight.gameObject.SetActive(false);
         BlacklightLight.gameObject.SetActive(false);
-
-        flCdResetTime = flashlightCooldown;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!flashlightEmpty && GameDataHolder.flashlightHasBeenPickedUp)
+        if (GameDataHolder.flashlightHasBeenPickedUp)
         {
-            //flashlight input effects start here
-            if (playerInput.actions["Flashlight"].triggered)
+            switch(state)
             {
-                if (!flashlightOn)
-                {
-                    if (PMS.inWater)
-                    {
-                        audioSource.PlayOneShot(flashlightInWaterSound);
-                    }
-                    else
-                    {
-                        audioSource.PlayOneShot(flashlightOutOfWaterSound);
-                    }
-                    FlashlightLight.gameObject.SetActive(true);
-                    emptyLight.gameObject.SetActive(false);
-                    onLight.gameObject.SetActive(true);
-                    flashlightOn = true;
-                }
-                else
-                {
-                    emptyLight.gameObject.SetActive(true);
-                    FlashlightLight.gameObject.SetActive(false);
-                    onLight.gameObject.SetActive(false);
-                    flashlightOn = false;
-                    BlacklightLight.gameObject.SetActive(false);
-                    blLight.gameObject.SetActive(false);
-                    Invoke("ClearUI", 4);
-                }
+                case State.flashlightOn:
+                    HandleFlashlightOn();
+                break;
+                case State.blacklightOn:
+                    HandleBlacklightOn();
+                break;
+                case State.flashlightOff:
+                    HandleFlashlightOff();
+                break;
             }
+        }
+    }
 
+    private void HandleFlashlightOn()
+    {
+        if (playerInput.actions["Flashlight"].triggered)
+        {
+            state = State.flashlightOff;
+        }
 
-            flashlightBattery = Mathf.Clamp(flashlightBattery, 0f, maxBattery);
+        FlashlightLight.gameObject.SetActive(true);
+        BlacklightLight.gameObject.SetActive(false);
+        emptyLight.gameObject.SetActive(false);
+        onLight.gameObject.SetActive(true);
+        blLight.gameObject.SetActive(false);
 
-            bool isBlacklightKeyHeld = playerInput.actions["Blacklight"].ReadValue<float>() > 0.1f;
-
-            if (flashlightOn && !flashlightDisable)
-            {
-                batteryBar.fillAmount = flashlightBattery/maxBattery;
-                if (isBlacklightKeyHeld)
-                {
-                    if (!blacklightIsOn)
-                    {
-                        blLight.gameObject.SetActive(true);
-                        audioSource.PlayOneShot(blacklightOnSound);
-                        blacklightIsOn = true;
-                    }
-                    flashlightBattery -= Time.deltaTime*2;
-                    BlacklightReveal();
-                }
-                else
-                {
-                    blacklightIsOn = false;
-                    blLight.gameObject.SetActive(false);
-                    flashlightBattery -= Time.deltaTime;
-                    BlacklightLight.gameObject.SetActive(false);
-                    FlashlightLight.gameObject.SetActive(true);
-                }
-            }
-            if (flashlightBattery <= 0)
-            {
-                flashlightOn = false;
-                flashlightText.text = "You ran out of battery";
-                emptyLight.gameObject.SetActive(true);
-                onLight.gameObject.SetActive(false);
-                blLight.gameObject.SetActive(false);
-                Invoke("ClearUI", 2);
-                flashlightEmpty = true;
-
-                flashlightDisable = true;
-            }
+        bool isBlacklightKeyHeld = playerInput.actions["Blacklight"].ReadValue<float>() > 0.1f;
+        if (isBlacklightKeyHeld)
+        {
+            blacklightBar.fillAmount += blacklightChargeTime * Time.deltaTime;
         }
         else
         {
-            BlacklightLight.gameObject.SetActive(false);
-            FlashlightLight.gameObject.SetActive(false);
+            blacklightBar.fillAmount -= blacklightChargeTime * Time.deltaTime;
         }
 
-        if(flashlightDisable)
+        if(blacklightBar.fillAmount == 1)
         {
-            FlCooldown();
+            state = State.blacklightOn;
+        }
+    }
+
+    private void HandleBlacklightOn()
+    {
+        blLight.gameObject.SetActive(true);
+        onLight.gameObject.SetActive(false);
+        BlacklightReveal();
+        blacklightBar.fillAmount -= blacklightDischargeTime * Time.deltaTime;
+        if(blacklightBar.fillAmount == 0)
+        {
+            state = State.flashlightOn;
+        }
+    }
+
+    private void HandleFlashlightOff()
+    {
+        if (GameDataHolder.flashlightHasBeenPickedUp)
+        {
+            emptyLight.gameObject.SetActive(true);
+        }
+        else
+        {
+            emptyLight.gameObject.SetActive(false);
+        }
+        blacklightBar.fillAmount -= blacklightChargeTime * Time.deltaTime;
+        if (playerInput.actions["Flashlight"].triggered)
+        {
+            if (PMS.inWater)
+            {
+                audioSource.PlayOneShot(flashlightInWaterSound);
+            }
+            else
+            {
+                audioSource.PlayOneShot(flashlightOutOfWaterSound);
+            }
+            state = State.flashlightOn;
         }
 
-        FlashlightUIControl();
-        //flashlight input effects end here
-
-
+        emptyLight.gameObject.SetActive(true);
+        FlashlightLight.gameObject.SetActive(false);
+        onLight.gameObject.SetActive(false);
+        BlacklightLight.gameObject.SetActive(false);
     }
     void BlacklightReveal()
     {
         BlacklightLight.gameObject.SetActive(true);
         FlashlightLight.gameObject.SetActive(false);
-        //if(Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit, range, layer))
         if(Physics.SphereCast(mainCam.transform.position, 1f, mainCam.transform.forward, out hit, range, layer) || Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit, range, layer))
         { 
             if (hit.collider.GetComponent<HiddenObjectsInteraction>() != false)
@@ -154,9 +144,7 @@ public class flashlightMechanic : MonoBehaviour
                 hI = hit.collider.GetComponent<HiddenObjectsInteraction>();
                 if (hI.objSpawned == false)
                 {
-                    flashlightText.text = "Object Revealed";
                     hI.objRevealed = true;
-                    Invoke("ClearUI", 3);
                 }
             }
 
@@ -165,9 +153,7 @@ public class flashlightMechanic : MonoBehaviour
                 rHO = hit.collider.GetComponent<RevealHiddenObjects>();
                 if (rHO.objSpawned == false)
                 {
-                    flashlightText.text = "Object Revealed";
                     rHO.objRevealed = true;
-                    Invoke("ClearUI", 3);
                 }
             }
 
@@ -176,9 +162,7 @@ public class flashlightMechanic : MonoBehaviour
                 sHO = hit.collider.GetComponent<SpawnHiddenObject>();
                 if (sHO.objSpawned == false)
                 {
-                    flashlightText.text = "Object Revealed";
                     sHO.objRevealed = true;
-                    Invoke("ClearUI", 3);
                 }
             }
             if (hit.collider.GetComponent<BlacklightEvent>() != false)
@@ -187,68 +171,5 @@ public class flashlightMechanic : MonoBehaviour
                 blEvent.uEvent.Invoke();
             }
         }
-    }
-    public void ClearUI()
-    {
-      flashlightText.text = "";
-    }
-
-
-    public void FillBattery(float amount)
-    {
-        flashlightBattery += amount;
-        if (flashlightBattery >= maxBattery)
-        {
-            flashlightBattery = maxBattery;
-        }
-        batteryBar.fillAmount = flashlightBattery/maxBattery;
-    }
-
-
-    void FlashlightUIControl()
-    {
-        if (GameDataHolder.flashlightHasBeenPickedUp == true)
-        {
-            flashlightUI.gameObject.SetActive(true);
-            flashlightContainers.gameObject.SetActive(true);
-            if (flashlightBattery >= maxBattery && !flashlightEmpty)
-            {
-                fullBatteryBar.gameObject.SetActive(true);
-                batteryBar.gameObject.SetActive(false);
-                emptyBatteryBar.gameObject.SetActive(false);
-            }
-            else if (flashlightBattery < maxBattery && !flashlightEmpty)
-            {
-                fullBatteryBar.gameObject.SetActive(false);
-                batteryBar.gameObject.SetActive(true);
-                emptyBatteryBar.gameObject.SetActive(true);
-            }
-            else
-            {
-                fullBatteryBar.gameObject.SetActive(false);
-                batteryBar.gameObject.SetActive(false);
-                emptyBatteryBar.gameObject.SetActive(true);
-            }
-        }
-        else
-        {
-            flashlightUI.gameObject.SetActive(false);
-        }
-    }
-
-    void FlCooldown()
-    {  
-        flashlightCooldown -= Time.deltaTime;
-        if(flashlightCooldown <= 0)
-        {
-            flashlightDisable = false;
-            flashlightCooldown = flCdResetTime;
-
-            FillBattery(maxBattery);
-            flashlightEmpty = false;
-
-            flashlightText.text = "Flashlight Battery Recharged";
-            Invoke("ClearUI", 3);
-        }   
     }
 }
