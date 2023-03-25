319df1165eac352f9c57fd235ee5c1c87da5de83
F-Alex-projects <alex@franquez.com>
2023-03-25 16:34:47 -0400

    cringeDetector nearly done

--- Assets/Scripts/Mechanics Scripts/SonarPingManager.cs
+++ Assets/Scripts/Mechanics Scripts/SonarPingManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SonarPingManager : MonoBehaviour
 {
     [SerializeField]private GameObject ping;
-    [SerializeField]private SpherePings managedPing;
+    [SerializeField]private SonarPings managedPing;
     private bool doneOnce;
     private bool instantiated;
 
@@ -20,6 +20,18 @@ public class SonarPingManager : MonoBehaviour
     {
         managedPing.SetDisappearTimer(time);
         Invoke("ResetBools", time);
+    }
+
+    public void ColorManager(int colorCase)
+    {
+        if (colorCase == 1)
+        {
+            managedPing.SetColor(new Color(0, 0, 1, 1));
+        }
+        if (colorCase == 2)
+        {
+            managedPing.SetColor(new Color(1, 0, 0, 1));
+        }
     }
 
     private void ResetBools()
--- Assets/Scripts/Mechanics Scripts/SonarPulse.cs
+++ Assets/Scripts/Mechanics Scripts/SonarPulse.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
@@ -9,24 +9,22 @@ public class SonarPulse : MonoBehaviour
     private SpriteRenderer pulseSpriteRenderer;
     private Color pulseColor;
 
-    private float range;
-    private float rangeMax;
-    private float fadeRange;
+    private float range, rangeMax, rangeSpeed, fadeRange, pingDelay;
 
     private List<Collider> collidersHit;
 
     private void Awake()
     {
-        rangeMax = 6f;
+        rangeMax = 15f;
         fadeRange = 1.5f;
+        rangeSpeed = 1.5f;
+        pingDelay = 1f;
         collidersHit = new List<Collider>();
         pulseSpriteRenderer = pulseTransform.GetComponent<SpriteRenderer>();
-
     }
 
     private void Update()
     {
-        float rangeSpeed = .25f;
         range += rangeSpeed * Time.deltaTime;
         if(range > rangeMax)
         {
@@ -35,7 +33,22 @@ public class SonarPulse : MonoBehaviour
         }
         
         pulseTransform.localScale = new Vector3(range, range);
-        Collider[] hitCollidersArray = Physics.OverlapSphere(pulseTransform.position, range * 100);
+        StartCoroutine(PingColliders());
+        if (range > rangeMax - fadeRange)
+        {
+            pulseColor.a = Mathf.Lerp(0f, 1f, (rangeMax - range) / fadeRange);
+        }
+        else
+        {
+            pulseColor.a = 1f;
+        }
+        pulseSpriteRenderer.color = new Color(pulseSpriteRenderer.color.r, pulseSpriteRenderer.color.g, pulseSpriteRenderer.color.b, pulseColor.a);
+        
+    }
+
+    IEnumerator PingColliders()
+    {
+        Collider[] hitCollidersArray = Physics.OverlapSphere(pulseTransform.position, range);
         foreach (Collider colliderHit in hitCollidersArray)
         {
             if (colliderHit != null && colliderHit.gameObject.GetComponent<SonarPingManager>())
@@ -45,20 +58,18 @@ public class SonarPulse : MonoBehaviour
                     collidersHit.Add(colliderHit);
                     SonarPingManager sPManager = colliderHit.GetComponent<SonarPingManager>();
                     sPManager.InstantiatePings();
+                    if (colliderHit.gameObject.layer == 24)
+                    {
+                        sPManager.ColorManager(1);
+                    }
+                    if (colliderHit.gameObject.layer == 10)
+                    {
+                        sPManager.ColorManager(2);
+                    }
                     sPManager.SetPingTimer(rangeMax/rangeSpeed);
                 }
             }
+            yield return new WaitForSeconds(pingDelay);
         }
-
-        if (range > rangeMax - fadeRange)
-        {
-            pulseColor.a = Mathf.Lerp(0f, 1f, (rangeMax - range) / fadeRange);
-        }
-        else
-        {
-            pulseColor.a = 1f;
-        }
-        pulseSpriteRenderer.color = pulseColor;
-        
     }
 }
