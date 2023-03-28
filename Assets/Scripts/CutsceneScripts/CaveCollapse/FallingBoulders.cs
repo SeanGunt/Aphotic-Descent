4f0420fb8cc50255bd2aa156c95ea9fcc5b1175e
skipper-PC\skipper <imtechnostatic@gmail.com>
2023-03-28 17:38:51 -0400

    Started Cave stuff, franky tail is flipped idk why

---
+++ Assets/Scripts/CutsceneScripts/CaveCollapse/FallingBoulders.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBoulders : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+
+    public static FallingBoulders Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool (string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        IPooledBoulders pooledBoulders = objectToSpawn.GetComponent<IPooledBoulders>();
+
+        if (pooledBoulders != null)
+        {
+            pooledBoulders.OnObjectSpawn();
+        }
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
---
+++ Assets/Scripts/CutsceneScripts/CaveCollapse/RotatingBoulders.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBoulders : MonoBehaviour, IPooledBoulders
+{
+    private float rotationSpeed = 5f;
+    public void OnObjectSpawn()
+    {
+        //Add rotation in here.
+    }
+}
