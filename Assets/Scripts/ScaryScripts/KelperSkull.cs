035220fc4e135b5f5624c5b00e3cb126040e943a
tacopuns <73323714+tacopuns@users.noreply.github.com>
2025-06-11 17:51:35 -0400

    moved keplers body + add skull rolling anim

---
+++ Assets/Scripts/ScaryScripts/KelperSkull.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KelperSkull : MonoBehaviour
+{
+    [SerializeField] private GameObject KeplerSkullObj;
+    [SerializeField] private Animator KeplerSkull;
+    //[SerializeField] private AudioClip scaryStinger;
+    //[SerializeField] private AudioSource audioSource;
+
+    void Start()
+    {
+        KeplerSkull = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("passing thru");
+            KeplerSkull.SetBool("playerIsNear", true);
+            Debug.Log("play anim");
+            //audioSource.PlayOneShot(scaryStinger);
+        }
+    }
+}
